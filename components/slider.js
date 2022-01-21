45a6fb8bb1724f64d6d0e4fd2a9ce1265dec4872
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-01-21 15:55:24 +0530

    slider html code added

---
+++ components/slider.js
@@ -0,0 +1,10 @@
+function sliderReview(){
+    return `<section class="image-slideshow">
+      <img class="slide-img" src="../Images/first-review.png" alt="" />
+      <img class="slide-img" src="../Images/second-review.png" alt="" />
+      <img class="slide-img" src="../Images/third-review.png" alt="" />
+    </section>`;
+}
+
+
+export default sliderReview;
