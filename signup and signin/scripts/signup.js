2c478f4194b4ad3655953b414311e9aecfab08a0
Kritika176 <kritikash944@gmail.com>
2022-01-21 18:13:21 +0530

    sign in

---
+++ signup and signin/scripts/signup.js
@@ -0,0 +1,40 @@
+
+
+document.querySelector("#create").addEventListener("click",Register)
+async function Register(event){
+    event.preventDefault();
+    try
+    {
+      var register_data={
+          name:document.getElementById('name').value,
+          email:document.getElementById("email").value,
+          username:document.getElementById("username").value,
+          password:document.getElementById("password").value,
+          mobile:document.getElementById("mobile").value,
+          description:document.getElementById("description").value,
+      }
+      register_data=JSON.stringify(register_data)
+      console.log(register_data);
+  }
+  catch(err)
+  {
+      console.log(err)
+  }
+  let reg_api = `https://masai-api-mocker.herokuapp.com/auth/register`
+  let response = await fetch(reg_api,{
+      method:'POST',
+      body:register_data,
+      headers:{
+          "Content-Type":'application/json'
+      },
+  });
+  let data = await response.json();
+  if(data.error == false)
+  {
+      window.location.href = "./login.html"
+  }
+  console.log(data);
+  }
+
+
+
